5d1274a4e41aabe373a7e0d51a124a6ef3a1b99a
Алексей А. Кузнецов <alexsmith_proff@mail.ru>
2024-05-02 08:35:57 +0300

    init storybook

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+interface ButtonProps {
+    children: React.ReactNode
+}
+
+const Button: FC<ButtonProps> = ({ children }) => {
+    return(
+        <button>{children}</button>
+    )
+}
+
+export default Button
