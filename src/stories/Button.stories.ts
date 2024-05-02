a74cec9cb6148f2d8d954ff8a318221fac7641a2
Алексей А. Кузнецов <alexsmith_proff@mail.ru>
2024-05-02 09:06:12 +0300

    st

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -26,7 +26,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    children: 'Кнопкааа'
+    children: 'Кнопка1'
     // primary: true,
     // label: 'Button',
   },
